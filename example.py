5a9db4427cd0ffea79c98f3294dbc0bd9c7ba0f4
Miao Jiang <miaojiang1987@gmail.com>
2018-09-17 18:16:38 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
-
+import os
+from PIL import Image
 import chainer
 from chainer import cuda
 from chainer import datasets
@@ -27,15 +28,35 @@ def main(args):
         model.to_gpu()
 
     # Load images
-    train, test = datasets.get_cifar10(ndim=3, withlabel=False, scale=255.0)
-
+#    train, test = datasets.get_cifar10(ndim=3, withlabel=False, scale=255.0)
+    names=os.listdir('sliced')
+    first_name=names[0]
+    im=Image.open('sliced/'+first_name)
+    im2arr=np.array(im,dtype=np.float32)
+    im2arr=im2arr[2:34,2:34,:]
+    im2arr_t=np.transpose(im2arr,(2,0,1))
+    
+    print(im2arr_t)
+    images=[im2arr_t]
+    for i in range(1,60000):
+        name=names[i]
+        im=Image.open('sliced/'+name)
+        im2arr=np.array(im,dtype=np.float32)
+        im2arr=im2arr[2:34,2:34,:]
+        
+                     
+        im2arr_t=np.transpose(im2arr,(2,0,1))
+        images.append(im2arr_t)
+    images_array=np.array(images)
+    print(images_array.shape)
     # Use all 60000 images, unless the number of samples are specified
-    ims = np.concatenate((train, test))
-    if args.samples > 0:
-        ims = ims[:args.samples]
+#    ims = np.concatenate((train, test))
+#    if args.samples > 0:
+#        images_array = images_array[:args.samples]
 
     with chainer.no_backprop_mode(), chainer.using_config('train', False):
-        mean, std = inception_score(model, ims)
+#        mean, std = inception_score(model, ims)
+         mean, std = inception_score(model, images_array)
 
     print('Inception score mean:', mean)
     print('Inception score std:', std)
